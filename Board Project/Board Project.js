522c208972e199b19834a43ee90c60e23f67741d
topoftheMorgan <77422567+topoftheMorgan@users.noreply.github.com>
2022-02-14 14:20:33 -0700

    Add files via upload

--- Board Project/Board Project.js
+++ Board Project/Board Project.js
@@ -1,6 +1,8 @@
 var x=0;
 var y=550;
-var y2=500;
+var y2=400;
+var y3=250;
+var y4=100;
 var img;
 var img2;
 //var sizeX=75;
@@ -8,6 +10,10 @@ var img2;
 var whaleX= 0;
 var whaleY= 0;
 var fishX= 0, fishY= 0;
+
+var timerValue= 5;
+var showImages= true;
+
 
 var speedX= 0;
 var speedY= 0;
@@ -20,13 +26,17 @@ var swimSpeedYs= [];
 
 var fishSize= 10;
 
-
+var initialX= 100;
 
 function preload() 
 {
     //img = loadImage('Assets/ocean.jpg');
-    whale = loadImage('Assets/images.jpeg');
-    Bfish = loadImage('Assets/fish.jpeg');
+    whale = loadImage('Assets/whale.png');
+    Bfish = loadImage('Assets/blue.png');
+    Ofish = loadImage('Assets/orange.png');
+    dory = loadImage('Assets/dory.png');
+    zeb = loadImage('Assets/fish.png')
+    bubble = loadImage('Assets/bubble.png')
 }
 
 let bg;
@@ -34,16 +44,24 @@ let yb= 0;
 
 function setup()
 {
-   
     speedX= random(1,10);
     spaadY= random(1,10);
+
+    setInterval(timeIt, 5000)
 
     //image(img, 0, 0); 
     bg= loadImage('Assets/ocean.jpg');
     createCanvas(980, 800);
+
+    /*for(var i=0; i<50; i++)
+    {
+        x[i]= initialX;
+        initialX +=10;
+    }*/
+
     background(40, 186, 237);
-    speedX= random(1,10);
-    speedY= random(1,10);
+    speedX= random(1,5);
+    speedY= random(1,5);
 
     var h = window.displayHeight;
     var w = window.displayWidth;
@@ -51,7 +69,7 @@ function setup()
     for(var i = 0; i < fishSize; i++)
     {
         //console.log(i)
-        fish[i] = Bfish;
+        fish[i] = bubble;
         fishXs[i] = fishX;
         fishYs[i] = fishY;
         fishX = random(0,w);
@@ -64,7 +82,37 @@ function setup()
 
 function draw()
 {
-    
+    /*for(var i=0; i<5; i++)
+    {
+        circel1= new mycircle(random(10,displayWidth),random(10, displayHeight))
+        mycircle[i]= circle1;
+    }*/
+
+    textSize(50);
+    text(timerValue, 100,100);   
+    if(timerValue > 0 && showImages == true)
+    {
+        for(var i = 0; i<fishXs.length; i++)
+        {
+            fishXs[i] = 0;
+        }
+        //console.log("a")
+            drawImages();
+    }
+
+    if(timerValue == 0)
+    {
+        timerValue = 5;
+        if(showImages == true)
+        {
+            showImages = false;
+        }
+        else
+        {
+            showImages = true;
+        }
+    }
+
     moveWhale();
     //image(img, 0, 0); 
     background(bg);
@@ -75,9 +123,12 @@ function draw()
     fill(196,157,78);
     rect(0,750, 870,250);*/
     
-    image(whale, whaleX,whaleY);
+    image(whale, whaleX,whaleY, 550, 350);
     //image(fish, 50,50);
-    image(Bfish, x,y);
+    image(Bfish, x,y, 125,75);
+    image(Ofish, x,y2, 125,75)
+    image(dory, x,y3, 125,75)
+    image(zeb, x,y4, 125,75)
 
     for(var i = 0; i < fish.length; i++)
     {
@@ -92,21 +143,22 @@ function draw()
     //name and title
     fill(0,0,0);
     textFont('cursive');
-    textSize('40');
-    text("Morgan Bowman",880,795);
-    text("Feeding Time",10,15);
+    textSize(20);
+    text("Morgan Bowman",830,790);
+    textSize(30);
+    text("Feeding Time",10,35);
     //mian fish
-    fill(240, 153, 0);
+    /*fill(240, 153, 0);
     ellipse(130,500, 100,65);
     ellipse(80,500, 30,75)
     fill(0,0,0);
     circle(168,510, 15);
     circle(153,490, 10);
     fill(255,255,255);
-    circle(152,489, 5);
+    circle(152,489, 5);*/
     //other fish
     fill(217, 234, 13);
-    ellipse(x,y2, 75,30);
+    //ellipse(x,y2, 75,30);
     //image(Bfish, 50,50);
 
     if(x >= 870)
@@ -120,8 +172,8 @@ function draw()
     }
      x= x+speedX;
      fill(234, 13, 137);
-     ellipse(x,y, 75,30);
-     if(x >= 870)
+     //ellipse(x,y, 75,30);
+    /* if(x >= 870)
     {
         speedX= random(1,10);
         speedX= -speedX;
@@ -130,8 +182,26 @@ function draw()
     {
         speedX = random(1,10);
         x= x+speedX;
-    }    
+    }    */
 }
+
+function drawImages()
+{
+    for(var i = 0; i < fishXs.length; i++)
+    {
+        console.log('a')
+        image(bubble,fishXs[i],10); 
+ 
+    }  
+}
+
+function timeIt() {
+    //console.log("a")
+    if (timerValue > 0) {   
+        timerValue--;
+    }
+}
+
 
 function moveWhale()
 {
---
+++ Board Project/mycircle.js
@@ -0,0 +1,14 @@
+class mycircle
+{
+    constructor(x, y, diameter)
+    {
+        this.x= x;
+        this.y= y;
+        this.diameter= diameter;
+    }
+
+    draw()
+    {
+        //circle1(this.x,this.y,this.diameter)
+    }
+}
